3875d73e8bf055cbcee320afdc68f7530e2d46f0
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-09-06 14:34:18 +0100

    Set up startStopGame function

---
+++ app.js
@@ -0,0 +1,33 @@
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM loaded');
+
+  let snake = document.querySelectorAll('.snake');
+  const startStopButton = document.getElementById('startStopButton');
+  const gameBoard = document.querySelector('.gameBoard');
+  let squares = gameBoard.querySelectorAll('div');
+  console.log(squares);
+
+  let direction = 0;
+  let snakeSpeed = 1000;
+  let interval;
+  let gameIsRunning = false;
+  console.log(snake);
+
+  const startStopGame = () => {
+    if (!gameIsRunning) {
+      gameIsRunning = true;
+      direction = 1;
+
+      interval = setInterval(() => {
+        console.log('Snake is moving');
+      }, snakeSpeed);
+    } else {
+      gameIsRunning = false;
+      console.log('clearing interval');
+      clearInterval(interval);
+    }
+  };
+
+  startStopButton.addEventListener('click', startStopGame);
+
+});
