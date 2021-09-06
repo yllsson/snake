2283b8accebf3ec430f69f44e868b8d01b1c3e8c
Ylva Turner <ylva.e.thorsen@gmail.com>
2021-09-06 15:11:20 +0100

    Add the remaining border crash options

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ window.addEventListener('DOMContentLoaded', () => {
   console.log(squares);
 
   let snakeHeadPosition = 0;
+  squares[snakeHeadPosition].classList.add('snake');
   const width = 10;
   let direction = 0;
   let snakeSpeed = 1000;
@@ -15,7 +16,7 @@ window.addEventListener('DOMContentLoaded', () => {
   const startStopGame = () => {
     if (!gameIsRunning) {
       gameIsRunning = true;
-      direction = 1;
+      direction += 1;
       startStopButton.innerText = 'Reset game';
 
       interval = setInterval(() => {
@@ -28,15 +29,17 @@ window.addEventListener('DOMContentLoaded', () => {
   };
 
   const resetGame = () => {
-    startStopButton.innerText = 'Start game';
-
+    clearInterval(interval);
     squares.forEach((square) => {
       square.classList.remove('snake');
     });
     gameIsRunning = false;
+    startStopButton.innerText = 'Start game';
     console.log('clearing interval');
-    clearInterval(interval);
+
     snakeHeadPosition = 0;
+    direction = 0;
+
     squares[snakeHeadPosition].classList.add('snake');
   };
 
@@ -47,13 +50,19 @@ window.addEventListener('DOMContentLoaded', () => {
       square.classList.remove('snake');
     });
 
-    if (snakeHeadPosition % width === 9 && direction === 1) {
+    console.log(snakeHeadPosition % width === 0 && direction === -1);
+
+    if (
+      (snakeHeadPosition % width === 9 && direction === 1) ||
+      (snakeHeadPosition % width === 0 && direction === -1) ||
+      (snakeHeadPosition <= 9 && direction === -10) ||
+      (snakeHeadPosition >= 90 && direction === 10)
+    ) {
       console.log('Crash!');
       resetGame();
     } else {
-      snakeHeadPosition += 1;
+      snakeHeadPosition += direction;
       squares[snakeHeadPosition].classList.add('snake');
-      console.log(snakeHeadPosition % width === 9 && direction === 1);
     }
   };
 });
